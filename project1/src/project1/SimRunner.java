f3faebe0d885955cc28d73e3b374986da3aff3f5
pengb1823 <33039063+pengb1823@users.noreply.github.com>
2020-02-09 19:13:35 -0800

    created runnable simulation of langevin dynamics

--- project1/src/project1/SimRunner.java
+++ project1/src/project1/SimRunner.java
@@ -4,19 +4,26 @@ import java.util.*;
 public class SimRunner {
 	public static void main(String[] args) {
 		
+		double mass = 39.948;
+		double epsilon = 0.0661;
+		double sigma = 0.3345;
+		double gamma = 0.1;
+		double b = 1.38064852e-23;
+		double temp = 1000;
+		double stepLength = 0.5;
 		int n;
-		int timestep = 10;
+		int stepNum = 500;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("how many atoms? :: ");
 		n = scan.nextInt();
 		//argon
-		VerletAlgorithm sim = new VerletAlgorithm(39.948, 0.0661, 0.3345, .1, 1.38064852e-23, n, timestep);
-		sim.setRandomCoord();
-		sim.printCoord(timestep);
-		sim.getForce();
-		
-		
+		VerletAlgorithm sim = new VerletAlgorithm(mass, epsilon, sigma, gamma, b, temp, stepLength, n, stepNum);
+		//sim.setRandomCoord();
+		sim.setPresetCoord();
+		sim.printCoord();
+		sim.setForce();
+		sim.getNewCoord();
 		
 		
 	}
--- project1/src/project1/VerletAlgorithm.java
+++ project1/src/project1/VerletAlgorithm.java
@@ -4,23 +4,30 @@ import java.util.*;
 import java.lang.Math;
 public class VerletAlgorithm {
 	
-	private double mass, epsilon, sigma, gamma, temp;
-	private int nAtoms, timestep;
+	private double mass, epsilon, sigma, gamma, temp, b, timestep;
+	private int nAtoms, steps;
 	private double[][] coord;
 	private double[][] coordPrime; // temp holding array for the next timestep while preserving the last timestep's coords for calculation of force
 	private double[][] momentum;
 	private double[][] force;
-	
+	Random ran = new Random(); 
 	
 	// constructor
-	public VerletAlgorithm(double m, double ep, double sig, double gam, double t, int n, int time) {
+	public VerletAlgorithm() {
+	}
+
+	public VerletAlgorithm(double m, double ep, double sig, double gam, double B, double t, double time, int n, int nStep) {
 		mass = m;
 		epsilon = ep;
 		sigma = sig;
 		gamma = gam;
 		temp = t;
+		b = B;
 		nAtoms = n;
 		timestep = time;
+		steps = nStep;
+		setMomentum();
+		setPrime();
 	}
 	
 	public double[][] getRandomCoord() {
@@ -35,11 +42,16 @@ public class VerletAlgorithm {
 		return coord;
 	}
 	
+	public double[][] getPresetCoord() {
+		double[][] coord = {{0.0, 0.0, 0.3}, {0.1, 0.2, -0.3}, {0.0, 0.0, 0.0}};
+		return coord;
+	}
+	
 	public double getLJForce(double r, double vector) {
 		if (r != 0) {
 			return ((48 * epsilon)/(r*r)) * ((Math.pow((sigma / r), 12)) - .5*(Math.pow((sigma / r), 6))) * vector;
 		}
-		return 0.0;
+		else return 0.0;
 	}
 	
 	public double[][] getForce() {
@@ -68,27 +80,57 @@ public class VerletAlgorithm {
 		return force;
 	}
 	
+	public double[][] getMomentum() {
+		double[][] momentum = new double[nAtoms][3];
+		for (int i = 0; i < nAtoms; i++) {
+			for (int j = 0; j < 3; j++) {
+				momentum[i][j] = 0.0;
+			}
+		}
+		return momentum;
+	}
+	
+	public double[][] getPrime() {
+		double[][] coordPrime = new double[nAtoms][3];
+		for (int i = 0; i < nAtoms; i++) {
+			for (int j = 0; j < 3; j++) {
+				coordPrime[i][j] = 0.0;
+			}
+		}
+		return coordPrime;
+	}
+	
+	public void setMomentum() {
+		momentum = getMomentum();
+	}
+	
 	public void setForce() {
 		force = getForce();
 	}
 	
-	public double[][] getNewCoord(double[][] coord) {
+	public void setPrime() {
+		coordPrime = getPrime();
+	}
+	
+	public double[][] getNewCoord() {
 		for (int i = 0; i < nAtoms; i++) {
-			
-			for (int j = 0; j < 3; i++) {
+			setForce();
+			for (int j = 0; j < 3; j++) {
 				momentum[i][j] = momentum[i][j] + 0.5 * timestep * force[i][j];
                 coord[i][j] = coord[i][j] + 0.5 * timestep * momentum[i][j] / mass;
-
+                coordPrime[i][j] = Math.exp(-gamma * timestep) * momentum[i][j] + Math.sqrt(1 - Math.exp(-2 * gamma * timestep)) * Math.sqrt(mass * b * temp) * (ran.nextGaussian() * .5 + .5);
+                coord[i][j] = coord[i][j] + .5 * timestep * coordPrime[i][j] / mass;
+                momentum[i][j] = coordPrime[i][j]  + .5 * timestep * force[i][j];
 			}
-			
 			
 		}
 		
-		
-		
-		
-		
-		
+		for (int i = 0; i < nAtoms; i++) {
+			for (int j = 0; j < 3; j++) {
+				setForce();
+				momentum[i][j] = coordPrime[i][j]  + .5 * timestep * force[i][j];
+			}
+		}
 		return coord;
 	}
 	
@@ -97,9 +139,13 @@ public class VerletAlgorithm {
 		coord = getRandomCoord();
 	}
 	
+	public void setPresetCoord() {
+		coord = getPresetCoord();
+	}
+	
 	//prints in .xyz file format
-	public void printCoord(int timestep) {
-		for (int k = 0; k < timestep; k++) {
+	public void printCoord() {
+		for (int k = 0; k < steps; k++) {
 			System.out.println(nAtoms);
 			System.out.println(k);
 			for (int i = 0; i < nAtoms; i++) {		
@@ -109,6 +155,7 @@ public class VerletAlgorithm {
 					if ((j + 1) % 3 == 0) System.out.print("\n");
 				}
 			}
+			getNewCoord();
 		}
 	}
 	
